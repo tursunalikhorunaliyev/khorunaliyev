f81b569d9653f40689052ad1a531ec9e4f76651f
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-03-10 18:03:19 +0500

    updated

--- src/main/java/com/khorunaliyev/khorunaliyev/TApplication.java
+++ src/main/java/com/khorunaliyev/khorunaliyev/TApplication.java
@@ -19,7 +19,7 @@ public class TApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/test/**")
-						.allowedOrigins("http://127.0.0.1:5500")
+						.allowedOrigins("http://127.0.0.1:5500/html/")
 						.allowedHeaders("Access-Control-Allow-Origin", "*")
 						.allowedHeaders("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
 						.allowedHeaders("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range")
